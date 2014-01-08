5085f3abbce7b3b314c605a81d2ccbb7d3a858d7
kevinjs <dysj4099@gmail.com>
2014-01-08 17:10:28 +0800

    bugfixed

--- monitor.py
+++ monitor.py
@@ -7,6 +7,7 @@ import sys, time, datetime
 import json
 
 from DaemonClass import Daemon
+from collections import OrderedDict
 import cpu
 import mem
 import load
@@ -16,31 +17,36 @@ import util
 class TestMonitor(Daemon):
     def __init__(self,
                pidfile='/tmp/test-monitor.pid',
-               stdin='/dev/null',
-               stdout='/dev/null',
-               stderr='/dev/null',
+               stdin='/dev/stdin',
+               stdout='/dev/stdout',
+               stderr='/dev/stderr',
                intvl=10,
-               logfile='/tmp/monitor.log'):
+               logfile='/opt/monitor.log'):
         Daemon.__init__(self, pidfile=pidfile, stdin=stdin, stdout=stdout, stderr=stderr)
         self._intvl = intvl
-        self.logfile = logfile
+        self._logfile = logfile
 
-    def poll():
+    def _poll(self):
         cpu_info = cpu.CPUInfo()
-        cpu_usage = cpu.CPUUsage()
+        cpu_usage = cpu.CPUUsage_all()
         mem_info = mem.MemInfo()
         net_info = net.NetStat()
         load_info = load.LoadStat()
-
-        poll_info = {}
-        poll_info['cpu_usage'] = cpu_usage
+    
+        poll_info = OrderedDict()
+        for cpu_i in cpu_usage:
+            poll_info[cpu_i] = cpu_usage[cpu_i]
+    
         poll_info['cpu_num'] = len(cpu_info)
         poll_info['mem_free'] = mem_info['MemFree']
         poll_info['mem_total'] = mem_info['MemTotal']
+        poll_info['mem_buffer'] = mem_info['Buffers']
+        poll_info['mem_cached'] = mem_info['Cached']
+        poll_info['mem_swapfree'] = mem_info['SwapFree']
         poll_info['load_1'] = load_info['load_1_min']
         poll_info['load_5'] = load_info['load_5_min']
-
         poll_info['load_15'] = load_info['load_15_min']
+    
         for net_i in net_info:
             poll_info[net_i+'_recv_bytes'] = net_info[net_i]['Receive']['bytes']
             poll_info[net_i+'_trans_bytes'] = net_info[net_i]['Transmit']['bytes']
@@ -49,21 +55,21 @@ class TestMonitor(Daemon):
         return poll_info
 
     def run(self):
+	c = 0
         while True:
-            chk_time = datetime.datetime.now()
-            poll_info = self.poll()
-            write_str = chk_time.strftime("%Y-%m-%dT%H:%M:%S") + '-----------\n'
+            poll_info = self._poll()
+            content = time.asctime(time.localtime()) + '\n'
             for item in poll_info:
-                write_str += '%s: %s\n' % (item, poll_info[item])
-            util.appendFile(write_str, self.logfile)
+                content += '%s: %s\n' %(item, poll_info[item])
+            content += '----------------------------\n\n'
+            util.appendFile(content, self._logfile)
             time.sleep(self._intvl)
+            c = c + 1
             
 if __name__ == "__main__":
-    daemon = TestMonitor(pidfile='/tmp/test-m.pid', 
-                           intvl=10, 
-                           logfile='/tmp/moni.log')
+    daemon = TestMonitor(pidfile='/tmp/test-monitor.pid', 
+                           intvl=10)
    
-    import pdb;pdb.set_trace() 
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
             daemon.start()

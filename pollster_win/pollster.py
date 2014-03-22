3e4bf8537d12d417ea9adc675ee0b1a6392bce1b
root <root@sj-dev.localdomain>
2014-03-22 09:51:05 +0800

    combine data

--- pollster_win/pollster.py
+++ pollster_win/pollster.py
@@ -5,37 +5,44 @@ import platform
 import time
 import win32com.client as client
 
-class DataPollster(object):
+class WinPollster(object):
+    _c = None
+    _cs = None
+    _os = None
+    _pfu = None
+    _com = None
+    _obj = None
+
+    hostname = None
+    ipaddress = None
+    def __init__(self):
+        self._c = wmi.WMI()
+        self._com = client.Dispatch("WbemScripting.SWbemRefresher")
+        self._obj = client.GetObject("winmgmts:\\root\cimv2")
+        self._cs = c.Win32_ComputerSystem()
+        self._os = c.Win32_OperatingSystem()
+        self._pfu = c.Win32_PageFileUsage()
+        self.hostname = self._os.CSName
 
     def get_cpu(self):
-        c = wmi.WMI()
         data_dict = {}
-        for cpu in c.Win32_Processor():
+        for cpu in self._c.Win32_Processor():
             device = cpu.DeviceID.lower()
             data_dict[device] = {'volume':float(cpu.LoadPercentage), 'unit':'%'}
         return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
 
     def get_mem(self):
-        c = wmi.WMI ()
-        cs = c.Win32_ComputerSystem() 
-        os = c.Win32_OperatingSystem() 
-        pfu = c.Win32_PageFileUsage()
-        
         data_dict = {}
-        data_dict["MemTotal"] = {'volume':float(cs[0].TotalPhysicalMemory) / (1024*1024), 'unit':'MB'}
-        data_dict["MemFree"] = {'volume':float(os[0].FreePhysicalMemory)/1024, 'unit':'MB'}
+        data_dict["MemTotal"] = {'volume':float(self._cs[0].TotalPhysicalMemory) / (1024*1024), 'unit':'MB'}
+        data_dict["MemFree"] = {'volume':float(self._os[0].FreePhysicalMemory)/1024, 'unit':'MB'}
         data_dict["Cached"] = {'volume':0, 'unit':'MB'}
         data_dict["Buffers"] = {'volume':0, 'unit':'MB'}
-        data_dict["SwapTotal"] = {'volume':float(pfu[0].AllocatedBaseSize), 'unit':'MB'}
-        data_dict["SwapFree"] = {'volume':float(pfu[0].AllocatedBaseSize - pfu[0].CurrentUsage), 'unit':'MB'}
+        data_dict["SwapTotal"] = {'volume':float(self._pfu[0].AllocatedBaseSize), 'unit':'MB'}
+        data_dict["SwapFree"] = {'volume':float(self._pfu[0].AllocatedBaseSize - self._pfu[0].CurrentUsage), 'unit':'MB'}
         return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
 
     def get_disk(self):
-        c = wmi.WMI ()
-
-        com = client.Dispatch("WbemScripting.SWbemRefresher")
-        obj = client.GetObject("winmgmts:\\root\cimv2")
-        diskitems = com.AddEnum(obj, "Win32_PerfFormattedData_PerfDisk_LogicalDisk").objectSet
+        diskitems = self._com.AddEnum(self._obj, "Win32_PerfFormattedData_PerfDisk_LogicalDisk").objectSet
 
         data_dict = {}
         data_dict['total_available'] = 0
@@ -43,7 +50,7 @@ class DataPollster(object):
         data_dict['total_free'] = 0
 
         #  DriveType=3 : "Local Disk",
-        for disk in c.Win32_LogicalDisk (DriveType=3):
+        for disk in self._c.Win32_LogicalDisk (DriveType=3):
             data_dict['total_available'] += round(float(disk.FreeSpace) / (1024*1024*1024), 2)
             data_dict['total_capacity'] += round(float(disk.Size) / (1024*1024*1024), 2)
             data_dict['total_free'] += round(float(disk.FreeSpace) / (1024*1024*1024), 2)
@@ -59,7 +66,7 @@ class DataPollster(object):
 
             data_dict[disk.DeviceID] = dev_tmp
 
-        com.Refresh()
+        self._com.Refresh()
         for item in diskitems:
             if item.Name in data_dict:
                 data_dict[item.Name]['io_stat'] = {}
@@ -70,14 +77,13 @@ class DataPollster(object):
         return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
 
     def get_net(self):
-        c = wmi.WMI ()
-        com = client.Dispatch("WbemScripting.SWbemRefresher")
-        obj = client.GetObject("winmgmts:\\root\cimv2")
-        items = com.AddEnum(obj, "Win32_PerfRawData_Tcpip_NetworkInterface").objectSet
+        items = self._com.AddEnum(obj, "Win32_PerfRawData_Tcpip_NetworkInterface").objectSet
 
         data_dict = {}
         interfaces = []
-        for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+        for interface in self._c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+            if interface.IPAddress[0]:
+                self.ipaddress = interface.IPAddress[0]
             interfaces.append(interface.Description)
 
         net_bytes_in = 0
@@ -85,10 +91,9 @@ class DataPollster(object):
         net_pkts_in = 0
         net_pkts_out = 0
         
-        com.Refresh()
+        self._com.Refresh()
         for item in items:
             if item.Name in interfaces:
-                #print 'Name:%s -> B_in:%s, B_out:%s, P_in:%s, P_out:%s' %(item.Name, item.BytesReceivedPerSec, item.BytesSentPerSec, item.PacketsReceivedPerSec, item.PacketsSentPerSec)
                 net_bytes_in += long(item.BytesReceivedPerSec)
                 net_bytes_out += long(item.BytesSentPerSec)
                 net_pkts_in += long(item.PacketsReceivedPerSec)
@@ -99,7 +104,7 @@ class DataPollster(object):
         net_bytes_in_cur = 0
         net_bytes_out_cur = 0
         
-        com.Refresh()
+        self._com.Refresh()
         for item in items:
             if item.Name in interfaces:
                 net_bytes_in = long(item.BytesReceivedPerSec) - net_bytes_in
@@ -118,18 +123,153 @@ class DataPollster(object):
 
         return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
 
-#    def get_pql(self):
-#        #Get ProcessorQueueLength
+    def combine(self):
+        combine_data = {}
+        combine_data['data'] = {}
+        combine_data['hostname'] = self.hostname
+        try:
+            combine_data['data']['CPUUsagePollster'] = self.get_cpu()
+            combine_data['data']['DiskUsagePollster'] = self.get_disk()
+            combine_data['data']['MemInfoPollster'] = self.get_mem()
+            combine_data['data']['NetStatPollster'] = self.get_net()
+
+            if self.ipaddress:
+                combine_data['ip_address'] = self.ipaddress
+            else:
+                combine_data['ip_address'] = '127.0.0.1'
+            combine_data['status'] = 'NORMAL'
+        except Exception,err:
+            combine_data['status'] = 'ERROR'
+        finally:
+            combine_data['timestamp'] = time.asctime(time.localtime()) 
+            return combine_data
+
+#class DataPollster(object):
+#
+#    def get_cpu(self):
+#        c = wmi.WMI()
+#        data_dict = {}
+#        for cpu in c.Win32_Processor():
+#            device = cpu.DeviceID.lower()
+#            data_dict[device] = {'volume':float(cpu.LoadPercentage), 'unit':'%'}
+#        return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
+#
+#    def get_mem(self):
+#        c = wmi.WMI ()
+#        cs = c.Win32_ComputerSystem() 
+#        os = c.Win32_OperatingSystem() 
+#        pfu = c.Win32_PageFileUsage()
+#        
+#        data_dict = {}
+#        data_dict["MemTotal"] = {'volume':float(cs[0].TotalPhysicalMemory) / (1024*1024), 'unit':'MB'}
+#        data_dict["MemFree"] = {'volume':float(os[0].FreePhysicalMemory)/1024, 'unit':'MB'}
+#        data_dict["Cached"] = {'volume':0, 'unit':'MB'}
+#        data_dict["Buffers"] = {'volume':0, 'unit':'MB'}
+#        data_dict["SwapTotal"] = {'volume':float(pfu[0].AllocatedBaseSize), 'unit':'MB'}
+#        data_dict["SwapFree"] = {'volume':float(pfu[0].AllocatedBaseSize - pfu[0].CurrentUsage), 'unit':'MB'}
+#        return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
+#
+#    def get_disk(self):
+#        c = wmi.WMI ()
+#
+#        com = client.Dispatch("WbemScripting.SWbemRefresher")
+#        obj = client.GetObject("winmgmts:\\root\cimv2")
+#        diskitems = com.AddEnum(obj, "Win32_PerfFormattedData_PerfDisk_LogicalDisk").objectSet
+#
+#        data_dict = {}
+#        data_dict['total_available'] = 0
+#        data_dict['total_capacity'] = 0
+#        data_dict['total_free'] = 0
+#
+#        #  DriveType=3 : "Local Disk",
+#        for disk in c.Win32_LogicalDisk (DriveType=3):
+#            data_dict['total_available'] += round(float(disk.FreeSpace) / (1024*1024*1024), 2)
+#            data_dict['total_capacity'] += round(float(disk.Size) / (1024*1024*1024), 2)
+#            data_dict['total_free'] += round(float(disk.FreeSpace) / (1024*1024*1024), 2)
+#
+#            dev_tmp = {}
+#            dev_tmp['dev'] = disk.DeviceID
+#            dev_tmp['available'] = {'volume':round(float(disk.FreeSpace) / (1024*1024*1024), 2), 'unit':'GB'}
+#            dev_tmp['capacity'] = {'volume':round(float(disk.Size) / (1024*1024*1024), 2), 'unit':'GB'}
+#            dev_tmp['free'] = {'volume':round(float(disk.FreeSpace) / (1024*1024*1024), 2), 'unit':'GB'}
+#            dev_tmp['fstype'] = disk.FileSystem
+#            dev_tmp['mnt'] = ''
+#            dev_tmp['used'] = round(long(disk.FreeSpace) / long(disk.Size), 2)
+#
+#            data_dict[disk.DeviceID] = dev_tmp
+#
+#        com.Refresh()
+#        for item in diskitems:
+#            if item.Name in data_dict:
+#                data_dict[item.Name]['io_stat'] = {}
+#                data_dict[item.Name]['io_stat']['r/s'] = {'volume':float(item.DiskReadsPerSec), 'unit':''}
+#                data_dict[item.Name]['io_stat']['w/s'] = {'volume':float(item.DiskWritesPerSec), 'unit':''}
+#                data_dict[item.Name]['io_stat']['rkB/s'] = {'volume':(float(item.DiskReadBytesPerSec) / 1024), 'unit':'KB/s'}
+#                data_dict[item.Name]['io_stat']['wkB/s'] = {'volume':(float(item.DiskWriteBytesPerSec) / 1024), 'unit':'KB/s'}
+#        return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
+#
+#    def get_net(self):
 #        c = wmi.WMI ()
 #        com = client.Dispatch("WbemScripting.SWbemRefresher")
 #        obj = client.GetObject("winmgmts:\\root\cimv2")
-#        items = com.AddEnum(obj, "Win32_PerfFormattedData_PerfOS_System").objectSet
+#        items = com.AddEnum(obj, "Win32_PerfRawData_Tcpip_NetworkInterface").objectSet
 #
+#        data_dict = {}
+#        interfaces = []
+#        for interface in c.Win32_NetworkAdapterConfiguration (IPEnabled=1):
+#            interfaces.append(interface.Description)
+#
+#        net_bytes_in = 0
+#        net_bytes_out = 0
+#        net_pkts_in = 0
+#        net_pkts_out = 0
+#        
 #        com.Refresh()
 #        for item in items:
-#            print item.ProcessorQueueLength
+#            if item.Name in interfaces:
+#                #print 'Name:%s -> B_in:%s, B_out:%s, P_in:%s, P_out:%s' %(item.Name, item.BytesReceivedPerSec, item.BytesSentPerSec, item.PacketsReceivedPerSec, item.PacketsSentPerSec)
+#                net_bytes_in += long(item.BytesReceivedPerSec)
+#                net_bytes_out += long(item.BytesSentPerSec)
+#                net_pkts_in += long(item.PacketsReceivedPerSec)
+#                net_pkts_out += long(item.PacketsSentPerSec)
+#
+#        time.sleep(1)
+#
+#        net_bytes_in_cur = 0
+#        net_bytes_out_cur = 0
+#        
+#        com.Refresh()
+#        for item in items:
+#            if item.Name in interfaces:
+#                net_bytes_in = long(item.BytesReceivedPerSec) - net_bytes_in
+#                net_bytes_in_cur += long(item.BytesReceivedPerSec)
+#                net_bytes_out = long(item.BytesSentPerSec) - net_bytes_out
+#                net_bytes_out_cur += long(item.BytesSentPerSec)
+#                net_pkts_in = long(item.PacketsReceivedPerSec) - net_pkts_in
+#                net_pkts_out = long(item.PacketsSentPerSec) - net_pkts_out
+#
+#        data_dict['net_bytes_in'] = {'volume':net_bytes_in, 'unit':'B/s'}
+#        data_dict['net_bytes_in_sum'] = {'volume':net_bytes_in_cur, 'unit':'B'}
+#        data_dict['net_bytes_out'] = {'volume':net_bytes_out, 'unit':'B/s'}
+#        data_dict['net_bytes_out_sum'] = {'volume':net_bytes_out_cur, 'unit':'B'}
+#        data_dict['net_pkts_in'] = {'volume':net_pkts_in, 'unit':'p/s'}
+#        data_dict['net_pkts_out'] = {'volume':net_pkts_out, 'unit':'p/s'}
+#
+#        return {'data':data_dict, 'timestamp':time.asctime(time.localtime())}
+#
+##    def get_pql(self):
+##        #Get ProcessorQueueLength
+##        c = wmi.WMI ()
+##        com = client.Dispatch("WbemScripting.SWbemRefresher")
+##        obj = client.GetObject("winmgmts:\\root\cimv2")
+##        items = com.AddEnum(obj, "Win32_PerfFormattedData_PerfOS_System").objectSet
+##
+##        com.Refresh()
+##        for item in items:
+##            print item.ProcessorQueueLength
 
 if __name__=='__main__':
-    dp = DataPollster()
-    print dp.get_net()
+    wp = WinPollster()
+    print wp.combine()
+    
     
